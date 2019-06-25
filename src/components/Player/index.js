ece694f85a5c4281633edb3444c3a242653e4a7d
Nicolas Paviot <nicolaspaviot@gmail.com>
2019-06-25 11:44:02 +0200

    now displaying the correct data for the two player, from redux's store

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -2,19 +2,19 @@
 import React from 'react';
 import PropsTypes from 'prop-types';
 
-const Player = () => (
+const Player = ({ player }) => (
   <div id="player">
-    <p>Firstname : Stan</p>
-    <p>Lastname : Wawrinka</p>
-    <p>shortname : S.WAW</p>
-    <p>Sex : M</p>
-    <img src='https://i.eurosport.com/_iss_/geo/country/flag/large/2213.png' alt='SUI' />
-    <img src='https://i.eurosport.com/_iss_/person/pp_clubteam/large/325225.jpg' alt='SUI' />
-    <p>rank: 21</p>
-    <p>points: 1784</p>
-    <p>weight: 81000</p>
-    <p>height: 183</p>
-    <p>age: 33</p>
+    <p>Firstname : {player.firstname}</p>
+    <p>Lastname : {player.lastname}</p>
+    <p>shortname : {player.shortname}</p>
+    <p>Sex : {player.sex}</p>
+    <img src={player.country.picture} alt={player.country.code} />
+    <img src={player.picture} alt="player" />
+    <p>rank: {player.rank}</p>
+    <p>points: {player.points}</p>
+    <p>weight: {player.weight}</p>
+    <p>height: {player.height}</p>
+    <p>age: {player.age}</p>
   </div>
 );
 
